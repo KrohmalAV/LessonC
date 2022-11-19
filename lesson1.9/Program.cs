c2783d6e4730bed8ff5bb0186b60fbe4adf3b8fa
KrohmalAV <Anton.avk@yandex.ru>
2022-11-19 21:45:23 +0300

    add lesson1.9 - Задача с друзьями и собакой

---
+++ lesson1.9/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+/* для случая когда данные введены пользователем с клавиатуры:
+Console.Write("Введите изначальное расстояние между друзьями в метрах: ");
+int distance = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите скорость движения первого друга: ");
+int speedfriend1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите скорость движения второго друга: ");
+int speedfriend2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите скорость движения собаки: ");
+int speeddog = Convert.ToInt32(Console.ReadLine());
+*/
+int distance = 10000;
+int speedfriend1 = 1;
+int speedfriend2 = 2;
+int speeddog = 5;
+
+int count = 0;
+int friend = 2;
+int time = 0;
+
+while(distance > 5)
+{
+    if (friend == 1)
+    {
+        time = distance / (speedfriend1 + speeddog);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (speedfriend2 + speeddog);
+        friend = 1;
+    }
+    distance = distance - (speedfriend1 + speedfriend2) * time;
+    count++;
+}
+Console.Write("Собака пробежит от одного друга к другому ");
+Console.Write(count);
+Console.WriteLine(" раз");
