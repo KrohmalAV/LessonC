a5e13ebf2ac6c3312248092e6067902fa6a227a0
KrohmalAV <Anton.avk@yandex.ru>
2022-11-19 19:14:42 +0300

    add lesson1.5 - случайные числа

---
+++ lesson1.5/Program.cs
@@ -0,0 +1,7 @@
+﻿double n = new Random().Next(1, 100);
+double m = new Random().Next(1, 11);
+Console.Write(n);
+Console.Write(" / ");
+Console.Write(m);
+Console.Write(" = ");
+Console.WriteLine(n / m);
