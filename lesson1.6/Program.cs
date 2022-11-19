f867b299bfd51984f9f17312865d012216eba0cd
KrohmalAV <Anton.avk@yandex.ru>
2022-11-19 19:35:36 +0300

    снова внесены изменения в lesson1.6 - учет разницы в регистрах

--- lesson1.6/Program.cs
+++ lesson1.6/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите ваше имя: ");
 string name = Console.ReadLine();
-if(name.ToLower == "антон")
+if(name.ToLower() == "антон")
 {
     Console.WriteLine("Добро пожаловать, хозяин!");
 }
