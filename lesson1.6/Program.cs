c95ae5b8e33a8160766303300a0a0ff33c35fbc4
KrohmalAV <Anton.avk@yandex.ru>
2022-11-19 19:31:19 +0300

    add lesson1.6 - ветвление

---
+++ lesson1.6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите ваше имя: ");
+string name = Console.ReadLine();
+if(name == "Антон")
+{
+    Console.WriteLine("Добро пожаловать, хозяин!");
+}
+else
+{
+    Console.Write("Здравствуйте, ");
+    Console.WriteLine(name);
+}
