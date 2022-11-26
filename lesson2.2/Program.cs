50a60999f3844f5c6ba7713ed57d3609eded20e7
KrohmalAV <Anton.avk@yandex.ru>
2022-11-26 22:27:48 +0300

    Add lesson2.2 - Поиск элемента в массиве

---
+++ lesson2.2/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {1, 7, 6, 4, 5, 6, 7, 8, 9};
+int n = array.Length;
+int i = 0;
+int find = 7;
+while(i < n)
+{
+    if(array[i] == find)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+    i++;
+}
